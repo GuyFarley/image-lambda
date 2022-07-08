0a391ca835174a66bde47d4784e974fbfe171f60
Guy Farley <guy.e.farley@gmail.com>
2022-07-07 18:29:49 -0700

    Updated code

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 const AWS = require('aws-sdk');
-
 const s3 = new AWS.S3();
 
 exports.handler = async (event) => {
-  console.log('event: ', event);
-  let bucketName = event.Records[0].s3.bucket.name;
-  let key = event.Records[0].s3.object.key;
-  let image = await s3.getObject({ Bucket: bucketName, Key: key }).promise();
-  console.log('image:', image);
 
-  let stringifiedImage = image.Body.toString();
-  let myImage = JSON.parse(stringifiedImage);
-  console.log('my image: ', myImage);
+  let bucketName = event.Records[0].s3.bucket.name;
+  let fileName = event.Records[0].s3.object.key;
+  let fileSize = event.Records[0].s3.object.size;
+
+  // console.log('event: ', bucketName, fileName, fileSize);
+
+  const params = {
+    Bucket: bucketName,
+    Key: 'images.json',
+  }
+
+  try {
+    const imageManifest = await s3.getObject(params).promise();
+    let manifestData = JSON.parse(imageManifest.Body.toString());
+    console.log('did this work? ', manifestData);
+    manifestData.push({
+      name: fileName,
+      size: fileSize,
+      type: 'image'
+    });
+    console.log(manifestData);
+
+    let manifestBody = JSON.stringify(manifestData);
+    const newManifest = await s3.putObject({ ...params, Body: manifestBody, ContentType: 'application' }).promise();
+
+    console.log('we got here');
+
+  } catch (e) {
+    console.log(e);
+
+    if (e.message === 'The specified key does not exist.') {
+      const newManifest = {
+        Bucket: bucketName,
+        Key: 'images.json',
+        Body: JSON.stringify([{ name: fileName, size: fileSize, type: 'image' }]),
+        ContentType: 'application',
+      };
+      const manifest = await s3.putObject(newManifest).promise();
+      console.log('JSON file created for bucket: ', manifest);
+    }
+    console.log('Need to create images.json');
+  }
+
+
   // TODO implement
   const response = {
     statusCode: 200,
